25bacaf385a1986bcf5e5782cffe1dfa6b001817
zhangxunan <zhang_xn1314@163.com>
2015-10-23 14:17:24 +0800

    架构修改

---
+++ zttx-platform/zttx-gen/src/main/java/com/zttx/web/gen/model/java/MethodParameter.java
@@ -0,0 +1,59 @@
+package com.zttx.web.gen.model.java;
+
+/**
+ * 方法参数对象
+ * <p>File：MethodParameter.java</p>
+ * <p>Title: MethodParameter</p>
+ * <p>Description:MethodParameter</p>
+ * <p>Copyright: Copyright (c) May 26, 2015</p>
+ * <p>Company: 8637.com</p>
+ * 
+ * @author Playguy
+ * @version 1.0
+ */
+public class MethodParameter
+{
+	/**
+	 * 实体对象中的GET/SET方法
+	 */
+	private JavaMethod	method;
+	/**
+	 * 参数类型
+	 */
+	private String		javaType;
+	/**
+	 * 方法名称
+	 */
+	private String		name;
+	
+	public JavaMethod getMethod()
+	{
+		return method;
+	}
+	
+	public void setMethod(JavaMethod method)
+	{
+		this.method = method;
+	}
+	
+	public String getJavaType()
+	{
+		return javaType;
+	}
+	
+	public void setJavaType(String javaType)
+	{
+		this.javaType = javaType;
+	}
+	
+	public String getName()
+	{
+		return name;
+	}
+	
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+	
+}
---
+++ zttx-platform/zttx-gen/src/main/java/com/zttx/web/gen/provider/JavaMethodProvider.java
@@ -0,0 +1,36 @@
+package com.zttx.web.gen.provider;
+
+import com.zttx.web.gen.IModelProvider;
+import com.zttx.web.gen.model.IModel;
+
+/**
+ * 实体类中方法的实现
+ * <p>File：JavaMethodProvider.java</p>
+ * <p>Title: JavaMethodProvider</p>
+ * <p>Description:JavaMethodProvider</p>
+ * <p>Copyright: Copyright (c) May 26, 2015</p>
+ * <p>Company: 8637.com</p>
+ * @author Playguy
+ * @version 1.0
+ */
+@SuppressWarnings("rawtypes")
+public class JavaMethodProvider implements IModelProvider
+{
+	
+	private String	methodName;
+	
+	private Class	clazz;
+	
+	public JavaMethodProvider(Class clazz, String methodName)
+	{
+		super();
+		this.clazz = clazz;
+		this.methodName = methodName;
+	}
+	
+	public IModel getModel()
+	{
+		return JavaModelHelper.getInstance().createJavaMethod(clazz, methodName);
+	}
+	
+}
